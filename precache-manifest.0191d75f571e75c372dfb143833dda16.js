c8717b2c9ed9ff1b17b595d083274d886933b877
Mulu B Haregot <mulbrotha@Mulus-MacBook-Pro.local>
2019-12-11 18:09:45 -0700

    Updates

---
+++ precache-manifest.0191d75f571e75c372dfb143833dda16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062d8beabc0a8ccb5d156e790cdd0391",
+    "url": "/habesha-giphy-collection/index.html"
+  },
+  {
+    "revision": "8c8848ebea3b245e0a98",
+    "url": "/habesha-giphy-collection/static/css/main.e2dede88.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/habesha-giphy-collection/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/habesha-giphy-collection/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c8848ebea3b245e0a98",
+    "url": "/habesha-giphy-collection/static/js/main.9161d46b.chunk.js"
+  },
+  {
+    "revision": "a4f2ee6acc8b88e9d380",
+    "url": "/habesha-giphy-collection/static/js/runtime-main.7c22af13.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/habesha-giphy-collection/static/media/logo.5d5d9eef.svg"
+  }
+]);
